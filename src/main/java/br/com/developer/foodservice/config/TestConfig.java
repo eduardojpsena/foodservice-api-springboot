83991c0ad4463e49e1e7f36cd27dbe5e7efcee9a
edujpsena <20201151021@iesp.edu.br>
2022-03-21 13:13:07 -0300

    Camada de serviços e implementação dos metodos getall findall e findbyid

--- src/main/java/br/com/developer/foodservice/config/TestConfig.java
+++ src/main/java/br/com/developer/foodservice/config/TestConfig.java
@@ -1,2 +1,26 @@
-package br.com.developer.foodservice.config;public class TestConfig {
+package br.com.developer.foodservice.config;
+
+import br.com.developer.foodservice.model.Usuario;
+import br.com.developer.foodservice.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Usuario u1 = new Usuario(1L, "eduardo", "1234");
+        Usuario u2 = new Usuario(2L, "marcelo", "4321");
+
+        usuarioRepository.saveAll(Arrays.asList(u1, u2));
+    }
 }
--- src/main/java/br/com/developer/foodservice/repository/UsuarioRepository.java
+++ src/main/java/br/com/developer/foodservice/repository/UsuarioRepository.java
@@ -1,2 +1,7 @@
-package br.com.developer.foodservice.repository;public interface UsuarioRepository {
+package br.com.developer.foodservice.repository;
+
+import br.com.developer.foodservice.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
